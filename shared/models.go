28ab2c7259694faeff30bcd80e7d0ebcc5115e8a
Saurav Gupta <er.sauravgpt@gmail.com>
2025-09-11 18:03:20 +0530

    added meta data

--- shared/models.go
+++ shared/models.go
@@ -1,5 +1,7 @@
 package shared
 
+import "time"
+
 type FileEvent struct {
 	Name      string `json:"name"`
 	Operation string `json:"operation"` // "create", "update", "delete"
@@ -8,4 +10,10 @@ type FileEvent struct {
 type APIResponse struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
+}
+
+type FileMetadata struct {
+	Name       string    `json:"name"`
+	Size       int64     `json:"size"`
+	ModifiedAt time.Time `json:"modifiedAt"`
 }
